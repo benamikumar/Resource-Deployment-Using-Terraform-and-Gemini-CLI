3060c28dac10e60ff338700a40c844cb5a760c3d
sammy856 <sameersingh856@gmail.com>
2026-05-30 17:47:18 +0530

    Add required_version to satisfy TFLint

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,14 +1,10 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=4.1.0"
     }
   }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = "643bfcc9-412c-4c3f-9588-d3206ea2210c"
 }
